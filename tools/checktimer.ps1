4d5e5da1211f180d4240275e83dcf044714e48a1
Friedrich von Never <friedrich@fornever.me>
2016-12-06 11:18:47 +0700

    #19: start script for Windows

---
+++ tools/checktimer.ps1
@@ -0,0 +1,30 @@
+param (
+    $CsvFile = "$(Resolve-Path ~)/checktimer.csv",
+
+    $SourceDir = "$PSScriptRoot/..",
+    $Jar = "$SourceDir/target/scala-2.11/checktimer_2.11-0.0.1.jar",
+    $MainClass = 'me.fornever.checktimer.Application',
+
+    $java = 'java',
+    $sbt = 'sbt'
+)
+
+$ErrorActionPreference = 'Stop'
+
+function exec($command) {
+    Write-Host "[checktimer exec] $command $args" -ForegroundColor White
+    & $command $args
+    if (!$?) {
+        throw "[checktimer error] $command $args = $LASTEXITCODE"
+    }
+}
+
+Push-Location $SourceDir
+try {
+    exec $sbt package
+    $classpath = exec $sbt 'export compile:dependencyClasspath' | Select-Object -Last 1
+
+    exec $java -cp "$classpath;$Jar" $MainClass $CsvFile
+} finally {
+    Pop-Location
+}
