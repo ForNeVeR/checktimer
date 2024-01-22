492d949f1a56a3ade44863a16b8d12ee0f048e2d
Friedrich von Never <friedrich@fornever.me>
2024-01-22 19:53:20 +0100

    Application: fix warnings of migration to ScalaFX 21

--- src/main/scala/me/fornever/checktimer/Application.scala
+++ src/main/scala/me/fornever/checktimer/Application.scala
@@ -3,8 +3,8 @@ package me.fornever.checktimer
 import me.fornever.checktimer.services.WindowServiceImpl
 import org.tinylog.scala.Logger
 import scalafx.Includes._
-import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.control.{Label, TextField, ToggleButton}
 import scalafx.scene.input.{KeyCode, KeyEvent, MouseEvent}
@@ -15,103 +15,103 @@ import scalafx.stage.StageStyle
 
 import java.nio.file.Paths
 
-object Application extends JFXApp {
+object Application extends JFXApp3 {
 
-  private val configuration = {
-    val configPath =
-      parameters.unnamed match {
-        case Seq(fileName) => fileName
-        case _ => Paths.get(Environment.homeDirectory, "checktimer.cfg").toString
+  override def start(): Unit = {
+    val configuration = {
+      val configPath =
+        parameters.unnamed match {
+          case Seq(fileName) => fileName
+          case _ => Paths.get(Environment.homeDirectory, "checktimer.cfg").toString
+        }
+      Configuration.loadFrom(configPath)
+    }
+
+    Logger.info("Arguments: {}", parameters.unnamed)
+    Logger.info("Configuration: {}", configuration)
+
+    stage = new PrimaryStage {
+      val windowService = new WindowServiceImpl(this)
+      val model = new ApplicationModel(Some(configuration.databasePath), windowService)
+
+      def keyPress(e: KeyEvent): Unit =
+        e match {
+          case _ if e.code == KeyCode.Enter =>
+            model.stop()
+            model.start(projectField.text.value, activityField.text.value)
+          case _ if e.code == KeyCode.Escape =>
+            model.stop()
+          case _ =>
+        }
+
+      private val stayOnTopButton = new ToggleButton("", Resources.pinIcon) {
+        tooltip = "Stay on Top"
+        selected.bindBidirectional(model.stayOnTop)
       }
-    Configuration.loadFrom(configPath)
+      private val projectField = new TextField {
+        onKeyPressed = keyPress _
+      }
+      private val activityField = new TextField {
+        onKeyPressed = keyPress _
+      }
+
+      title = "checktimer"
+      scene = new Scene {
+        stylesheets += getClass.getResource("/style.css").toExternalForm
+
+        private val em = Font.default.getSize
+        HBox.setMargin(stayOnTopButton, Insets(0, 0, 0, -0.5 * em))
+
+        root = new VBox {
+          children = Seq(
+            new HBox {
+              alignment = Pos.Center
+              children = Seq(
+                stayOnTopButton,
+                new Label("Project: ") {
+                  styleClass += "project-label"
+                },
+                projectField,
+                new Label(" Activity: "),
+                activityField
+              )
+            },
+            new HBox {
+              alignment = Pos.CenterRight
+              children = Seq(
+                new Label("Current: ") {
+                  minWidth = Region.USE_PREF_SIZE
+                },
+                new Label {
+                  styleClass += "changeable-text"
+                  text <== model.currentProjectInfo
+                },
+                new Label(", timing: ") {
+                  minWidth = Region.USE_PREF_SIZE
+                },
+                new Label {
+                  styleClass ++= Seq("changeable-text")
+                  text <== model.currentTimeString
+                  minWidth = Region.USE_PREF_SIZE
+                }
+              )
+            }
+          )
+        }
+
+        onMousePressed = saveMousePosition _
+        onMouseReleased = removeMousePosition _
+        onMouseDragged = moveWindow _
+
+        cursor = Cursor.Move
+      }
+
+      onCloseRequest = _ => model.stop()
+    }
+
+    stage.initStyle(StageStyle.Undecorated)
+    stage.icons.add(Resources.checktimerIcon)
   }
-
-  Logger.info("Arguments: {}", parameters.unnamed)
-  Logger.info("Configuration: {}", configuration)
-
-  stage = new PrimaryStage {
-    val windowService = new WindowServiceImpl(this)
-    val model = new ApplicationModel(Some(configuration.databasePath), windowService)
-
-    def keyPress(e: KeyEvent): Unit =
-      e match {
-        case _ if e.code == KeyCode.Enter =>
-          model.stop()
-          model.start(projectField.text.value, activityField.text.value)
-        case _ if e.code == KeyCode.Escape =>
-          model.stop()
-        case _ =>
-      }
-
-    private val stayOnTopButton = new ToggleButton("", Resources.pinIcon) {
-      tooltip = "Stay on Top"
-      selected.bindBidirectional(model.stayOnTop)
-    }
-    private val projectField = new TextField {
-      onKeyPressed = keyPress _
-    }
-    private val activityField = new TextField {
-      onKeyPressed = keyPress _
-    }
-
-    title = "checktimer"
-    scene = new Scene {
-      stylesheets += getClass.getResource("/style.css").toExternalForm
-
-      private val em = Font.default.getSize
-      HBox.setMargin(stayOnTopButton, Insets(0, 0, 0, -0.5 * em))
-
-      root = new VBox {
-        children = Seq(
-          new HBox {
-            alignment = Pos.Center
-            children = Seq(
-              stayOnTopButton,
-              new Label("Project: ") {
-                styleClass += "project-label"
-              },
-              projectField,
-              new Label(" Activity: "),
-              activityField
-            )
-          },
-          new HBox {
-            alignment = Pos.CenterRight
-            children = Seq(
-              new Label("Current: ") {
-                minWidth = Region.USE_PREF_SIZE
-              },
-              new Label {
-                styleClass += "changeable-text"
-                text <== model.currentProjectInfo
-              },
-              new Label(", timing: ") {
-                minWidth = Region.USE_PREF_SIZE
-              },
-              new Label {
-                styleClass ++= Seq("changeable-text")
-                text <== model.currentTimeString
-                minWidth = Region.USE_PREF_SIZE
-              }
-            )
-          }
-        )
-      }
-
-      onMousePressed = saveMousePosition _
-      onMouseReleased = removeMousePosition _
-      onMouseDragged = moveWindow _
-
-      cursor = Cursor.Move
-    }
-
-    onCloseRequest = handle {
-      model.stop()
-    }
-  }
-
-  stage.initStyle(StageStyle.Undecorated)
-  stage.icons.add(Resources.checktimerIcon)
 
   private var baseMousePosition: Option[(Double, Double)] = None
 
--- src/main/scala/me/fornever/checktimer/services/WindowService.scala
+++ src/main/scala/me/fornever/checktimer/services/WindowService.scala
@@ -1,13 +1,13 @@
 package me.fornever.checktimer.services
 
-import scalafx.application.JFXApp.PrimaryStage
+import scalafx.stage.Stage
 
 trait WindowService {
 
   def stayOnTop(state: Boolean): Unit
 }
 
-class WindowServiceImpl(stage: PrimaryStage) extends WindowService {
+class WindowServiceImpl(stage: Stage) extends WindowService {
 
   override def stayOnTop(state: Boolean): Unit = {
     stage.setAlwaysOnTop(state)
