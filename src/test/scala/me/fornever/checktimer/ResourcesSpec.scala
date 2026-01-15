df7227b61825c2dcd6b38a68c0b9dca8bc8d4d38
Friedrich von Never <friedrich@fornever.me>
2026-01-16 00:36:53 +0100

    Tests: disable SVG rendering on Linux

--- src/test/scala/me/fornever/checktimer/ResourcesSpec.scala
+++ src/test/scala/me/fornever/checktimer/ResourcesSpec.scala
@@ -9,7 +9,10 @@ import org.scalatest.matchers.should.Matchers
 
 class ResourcesSpec extends AnyFlatSpec with Matchers {
 
+  private val disableUiTest = sys.env.get("DISABLE_UI_TEST").contains("true")
+
   "Resources" should "load pinIcon successfully" in {
+    assume(!disableUiTest, "UI tests are disabled on this platform")
     val icon = Resources.pinIcon
     icon should not be null
   }
